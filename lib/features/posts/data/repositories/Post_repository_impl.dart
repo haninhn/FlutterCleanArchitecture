17ea28dbb285ca8cc43594d4096f12deb71f7870
HanineGuesmi <guesmihanin@takiacademyteam.com>
2023-01-10 23:59:53 +0100

    handel error

--- lib/features/posts/data/repositories/Post_repository_impl.dart
+++ lib/features/posts/data/repositories/Post_repository_impl.dart
@@ -15,58 +15,49 @@ class PostRepositoryImpl implements Postsrepository {
   final PostRemoteDateSource remoteDateSource;
   final PostLocalDateSource localDateSource;
   final NetworkInfo networkInfo;
-  PostRepositoryImpl({
-    required this.remoteDateSource, 
-  required this.localDateSource, 
-  required this.networkInfo});
+  PostRepositoryImpl(
+      {required this.remoteDateSource,
+      required this.localDateSource,
+      required this.networkInfo});
 
   @override
-  Future<Either<Failure, Unit>> addPost(Post post) async {  
-    final PostModel postModel = PostModel(title: post.title, body: post.body, id: post.id);
-    if(await networkInfo.isConnected){     
-      try{
-        await remoteDateSource.addPost(postModel);
-        return const Right(unit);
+  Future<Either<Failure, Unit>> addPost(Post post) async {
+    final PostModel postModel =
+        PostModel(title: post.title, body: post.body, id: post.id);
+    return await _getMessage(() {
+      return remoteDateSource.addPost(postModel);
+    });
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deletePost(int id) async {
+    return await _getMessage(() {
+      return remoteDateSource.deletePost(id);
+    });
+  }
+
+  @override
+  Future<Either<Failure, Unit>> updatePost(Post post) async {
+    final PostModel postModel =
+        PostModel(id: post.id, title: post.title, body: post.body);
+    return await _getMessage(() {
+      return remoteDateSource.updatePost(postModel);
+    });
+  }
+
+  @override
+  Future<Either<Failure, List<Post>>> getAllPosts() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePosts = await remoteDateSource.getAllPosts();
+        localDateSource.cachedPosts(remotePosts);
+        return Right(remotePosts);
       } on ServerException {
         return Left(ServerFailure());
-      } 
-    }else{
-      return Left(OfflineFailure());
-    }
-  
-  }
-
-  @override
-  Future<Either<Failure, Unit>> deletePost(int id)async {
-    if(await networkInfo.isConnected){  
-      try{
-       await remoteDateSource.deletePost(id);
-       return Right(unit);
-
-      }on  ServerException {
-        return Left(ServerFailure());
-      } 
-    }else{
-
-
-    }
-
-  }
-
-  @override
-  Future<Either<Failure, List<Post>>> getAllPosts() async{
-    if(await networkInfo.isConnected){ 
-      try{
-        final remotePosts = await remoteDateSource.getAllPosts();
-        localDateSource.cachedPosts(remotePosts);   
-        return Right(remotePosts);
-      }on ServerException{
-        return Left(ServerFailure());
       }
-
-    }else{
-      try{
-        final localPosts=  await localDateSource.getCachedPosts();
+    } else {
+      try {
+        final localPosts = await localDateSource.getCachedPosts();
         return Right(localPosts);
       } on ServerException {
         return Left(EmptyCasheFailure());
@@ -74,15 +65,19 @@ class PostRepositoryImpl implements Postsrepository {
     }
   }
 
-  @override
-  Future<Either<Failure, Unit>> updatePost(Post post) async {
-    final PostModel postModel =
-        PostModel(id: post.id, title: post.title, body: post.body);
-
-    await remoteDateSource.updatePost(postModel);
-        throw UnimplementedError();
+  //methode to handel errors
+  Future<Either<Failure, Unit>> _getMessage(
+      Future<Unit> Function() deleteOrUpdateOrAddPost) async {
+    if (await networkInfo.isConnected) {
+      try {
+        //function
+        await deleteOrUpdateOrAddPost();
+        return const Right(unit);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(OfflineFailure());
+    }
   }
-
 }
-
-
